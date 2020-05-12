a62343af1358b1d3bc3577e43a42db84014949fa
cam <cam@cams-air.home>
2020-05-12 10:41:04 +0100

    felix

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          default public!
+          felix default public!
         </p>
         <a
           className="App-link"
